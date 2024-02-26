48eafffee0c6b95cdd1b77ef8ef7df6625b5c474
Fanxuan <45900781+fzdesign@users.noreply.github.com>
2024-02-26 17:38:32 -0500

    2/26_office hour

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -4,11 +4,15 @@ import json
 
 parsed_json = json.load(open('data/fanxuanz_cc1.json'))
 level_pack = cc_classes.CCLevelPack()
-level=cc_classes.CCLevel()
-level.level_number = 1 #replace with parsed json
-level.time = level_time=60
-level.level_pack = level_pack
-
+level= cc_classes.CCLevel()
+level.level_number = parsed_json['levels'][0]['level_number']
+level.time = level_time= parsed_json['levels'][0]['time']
+level.num_chips= parsed_json['levels'][0]['num_chips']
+level.upper_layer= parsed_json['levels'][0]['upper_layer']
+level.add_field(cc_classes.CCEncodedPasswordField(parsed_json['levels'][0]['password']))
+level.add_field(cc_classes.CCMapHintField(parsed_json['levels'][0]['hint']))
+level.add_field(cc_classes.CCMapTitleField(parsed_json['levels'][0]['title']))
+#level.add_field(cc_classes.CCCoordinate(parsed_json['levels'][0]['monster']))
 level_pack.add_level(level)
 cc_dat_utils.write_cc_level_pack_to_dat(level_pack, "my.dat")
 
