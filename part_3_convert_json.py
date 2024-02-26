a09e100cc18644f337ea92425192eb9b3d168ead
Fanxuan <45900781+fzdesign@users.noreply.github.com>
2024-02-26 09:08:58 -0500

    Chip-tools

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -1,6 +1,20 @@
+import cc_classes
 import cc_dat_utils
+import json
 
-#Part 3
-#Load your custom JSON file
-#Convert JSON data to CCLevelPack
-#Save converted data to DAT file
+parsed_json = json.load(open('data/fanxuanz_cc1.json'))
+level_pack = cc_classes.CCLevelPack()
+level=cc_classes.CCLevel()
+level.level_number = 1 #replace with parsed json
+level.time = level_time=60
+level.level_pack = level_pack
+
+level_pack.add_level(level)
+cc_dat_utils.write_cc_level_pack_to_dat(level_pack, "my.dat")
+
+
+
+
+
+
+
