50979b54bf42fc0a7183b0e2164a8bfd77ea257d
Fanxuan <45900781+fzdesign@users.noreply.github.com>
2024-03-03 13:51:15 -0500

    3/3/2024

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -2,7 +2,7 @@ import cc_classes
 import cc_dat_utils
 import json
 
-parsed_json = json.load(open('data/fanxuanz_cc1.json'))
+parsed_json = json.load(open('data/fanxuanz_chipschallenge.json'))
 level_pack = cc_classes.CCLevelPack()
 level= cc_classes.CCLevel()
 level.level_number = parsed_json['levels'][0]['level_number']
@@ -13,6 +13,9 @@ level.add_field(cc_classes.CCEncodedPasswordField(parsed_json['levels'][0]['pass
 level.add_field(cc_classes.CCMapHintField(parsed_json['levels'][0]['hint']))
 level.add_field(cc_classes.CCMapTitleField(parsed_json['levels'][0]['title']))
 #level.add_field(cc_classes.CCCoordinate(parsed_json['levels'][0]['monster']))
+#monsters = [cc_classes.CCCoordinate(monster["x"], monster["y"]) for monster in field["monster"]]
+level.add_field(cc_classes.CCMonsterMovementField(parsed_json['levels'][1]['monster']))
+
 level_pack.add_level(level)
 cc_dat_utils.write_cc_level_pack_to_dat(level_pack, "my.dat")
 
