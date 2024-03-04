5b9b1767b27cb48cd8783046985b1ed6fc34c99c
Fanxuan <45900781+fzdesign@users.noreply.github.com>
2024-03-03 21:14:34 -0500

    3/3/2024_Tile_World

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -4,19 +4,21 @@ import json
 
 parsed_json = json.load(open('data/fanxuanz_chipschallenge.json'))
 level_pack = cc_classes.CCLevelPack()
-level= cc_classes.CCLevel()
-level.level_number = parsed_json['levels'][0]['level_number']
-level.time = level_time= parsed_json['levels'][0]['time']
-level.num_chips= parsed_json['levels'][0]['num_chips']
-level.upper_layer= parsed_json['levels'][0]['upper_layer']
-level.add_field(cc_classes.CCEncodedPasswordField(parsed_json['levels'][0]['password']))
-level.add_field(cc_classes.CCMapHintField(parsed_json['levels'][0]['hint']))
-level.add_field(cc_classes.CCMapTitleField(parsed_json['levels'][0]['title']))
+for level_j in parsed_json['levels']:
+    level= cc_classes.CCLevel()
+    level.level_number = level_j['level_number']
+    level.time = level_time= level_j['time']
+    level.num_chips= level_j['numberOfChips']
+    level.upper_layer= level_j['upperLayer']
+    level.add_field(cc_classes.CCEncodedPasswordField(level_j['password']))
+    level.add_field(cc_classes.CCMapHintField(level_j['hintText']))
+    level.add_field(cc_classes.CCMapTitleField(level_j['title']))
+
 #level.add_field(cc_classes.CCCoordinate(parsed_json['levels'][0]['monster']))
 #monsters = [cc_classes.CCCoordinate(monster["x"], monster["y"]) for monster in field["monster"]]
-level.add_field(cc_classes.CCMonsterMovementField(parsed_json['levels'][1]['monster']))
+#level.add_field(cc_classes.CCMonsterMovementField(parsed_json['levels'][1]['monster']))
 
-level_pack.add_level(level)
+    level_pack.add_level(level)
 cc_dat_utils.write_cc_level_pack_to_dat(level_pack, "my.dat")
 
 
